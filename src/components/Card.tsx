588acb77071f237b64a39bbb10afac77e64290a7
elmelech <roey.elm@gmail.com>
2024-05-20 11:34:50 -0400

    fix href issue

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -74,7 +74,7 @@ Card.Title = function CardTitle<T extends React.ElementType = 'h2'>({
 
   return (
     <Component className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-      {href ? <Card.Link>{children}</Card.Link> : children}
+      {href ? <Card.Link href={href}>{children}</Card.Link> : children}
     </Component>
   )
 }
